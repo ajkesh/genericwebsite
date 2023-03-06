10c104cd0890897617a4c2ac5a5904db055fec12
Armon Keshmiri <ajkeshmiri@gmail.com>
2023-03-05 20:26:46 -0800

    started password protection

---
+++ app.js
@@ -0,0 +1,9 @@
+function checkPassword(){
+    let passkey = document.getElementByID("password");
+if(passkey.onclick === "pass"){
+    location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+}
+    else{
+
+    }
+}
